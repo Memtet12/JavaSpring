57257821411b305f841538d7d460cab34e02d17e
Andrey <eldotefc@gmail.com>
2025-02-01 14:39:39 +0500

    Добавил Даты и css

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,15 +1,19 @@
 package com.example.demo.model;
 
-import javax.swing.*;
+import java.util.Date;
+
 
 public class Post {
     private String text;
     private  Integer likes;
+    private Date creationDate;
 
-    public Post(String text)
+    public Post(String text, Date creationDate)
     {
         this.text = text;
+        this.creationDate = creationDate;
     }
+
 
     public String getText()
     {
@@ -25,4 +29,9 @@ public class Post {
     {
         this.likes = likes;
     }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
 }
--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -1,19 +1,22 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Post;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-
-@org.springframework.stereotype.Service
+@Service
 public class PostService {
-    public List<Post> listAllPosts()
-    {
+    public List<Post> listAllPosts() {
         List<Post> posts = new ArrayList<>();
-        posts.add(new Post("Первый пост - я впервые в интернете, вроде все хорошие"));
-        posts.add(new Post("Второй пост - я пытался познакомиться с кем-то, но мне написали, что я скучный("));
-        posts.add(new Post("Третий пост - мне написал какой-то парень и предложил поиграть в майнкрафт вместе, я рад:3"));
+
+        // Создаём посты с текущей датой
+        posts.add(new Post("Первый пост - я впервые в интернете, вроде все хорошие", new Date(125,1,1)));
+        posts.add(new Post("Второй пост - я пытался познакомиться с кем-то, но мне написали, что я скучный(",new Date(125,1,1)));
+        posts.add(new Post("Третий пост - мне написал какой-то парень и предложил поиграть в майнкрафт вместе, я рад:3",new Date(125,1,1)));
+
         return posts;
     }
 }
