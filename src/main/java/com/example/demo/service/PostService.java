060808b3665c4c6fe7314f190361f8ebba1b22af
Andrey <eldotefc@gmail.com>
2025-02-01 15:00:18 +0500

    6.3 Добавил css, забыл в прошлый раз

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -12,7 +12,6 @@ public class PostService {
     public List<Post> listAllPosts() {
         List<Post> posts = new ArrayList<>();
 
-        // Создаём посты с текущей датой
         posts.add(new Post("Первый пост - я впервые в интернете, вроде все хорошие", new Date(125,1,1)));
         posts.add(new Post("Второй пост - я пытался познакомиться с кем-то, но мне написали, что я скучный(",new Date(125,1,1)));
         posts.add(new Post("Третий пост - мне написал какой-то парень и предложил поиграть в майнкрафт вместе, я рад:3",new Date(125,1,1)));
